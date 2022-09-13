6d74f983a571d583e6a181a25d28990ed202ffbc
Kevin Kimeto <m.officialkev@gmail.com>
2022-09-13 08:52:25 -0700

    Axios with json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 function App() {
+
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get(url)
+        .then((res) => {
+            setItems(res.data);
+            console.log(res.data);
+        });
+    }, []);
+
+    const result = items.map((id, title) => {
+        return (
+            id= 'id',
+            title='title'
+        );
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>{result.title}</h1>
     </div>
   );
 }
